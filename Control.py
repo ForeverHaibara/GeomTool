5bf4a0446e5f9c0eb9d8868cbdcf822c733bfe24
jjdishere <emailboxofjjd@163.com>
2024-02-27 16:03:04 +0800

    init control

---
+++ Control.py
@@ -0,0 +1,20 @@
+import random, math
+
+class ConstructionAction:
+    def __init__(self,in_name, in_gen_type, in_item_type) -> None:
+        pass
+
+    def generate(self, in_fun, in_check, in_errorinfo):
+        pass
+
+    def generate_check_triv(self, in_fun):
+        pass
+
+class DependencyAction:
+    def __init__(self) -> None:
+        pass
+
+class FullAction(ConstructionAction):
+
+## we need function input string output Construction + Dependency
+
