31670da831d3f6744c8cfbcdd4a537b6fa5263c9
cybcatppuccino <1538580589@qq.com>
2024-03-05 12:40:48 +0800

    Update GradientDescent.py

--- GradientDescent.py
+++ GradientDescent.py
@@ -13,9 +13,11 @@ def get_value(vl, vnl):
     return outlst
 
 def cyclic_det(x1, y1, x2, y2, x3, y3):
+    # Determine whether the four points (0, 0), (xi, yi) are cyclic
     return x2**2 * (x3 * y1 - x1 * y3) + x2 * (-x3**2 * y1 + y3 * (x1**2 + y1**2 - y1 * y3)) + y2 * (-x1**2 * x3 + x3 * y1 * (-y1 + y2) + x1 * (x3**2 - y2 * y3 + y3**2))
 
 def calc_datum(descent_datum, in_geom_list):
+    # Calculate the descent datum. -- To minimize what? 
     outsum = 0
     for data in descent_datum:
         if data[0] == "eq":
@@ -26,6 +28,7 @@ def calc_datum(descent_datum, in_geom_list):
             if data[1].type == "Line":
                 outsum += (data[1].c[0] * data[2].c[1] - data[2].c[0] * data[1].c[1]) **2 + (data[1].c[1] * data[2].c[2] - data[2].c[1] * data[1].c[2]) **2 + (data[1].c[2] * data[2].c[0] - data[2].c[2] * data[1].c[0]) **2
             continue
+
         if data[0] == "eqdist":
             if data[1].type == "Line":
                 d1 = (data[1].c[0] * data[2].c[0] + data[1].c[1] * data[2].c[1] + data[1].c[2])**2 / (data[1].c[0] ** 2 + data[1].c[1] ** 2)
@@ -41,30 +44,48 @@ def calc_datum(descent_datum, in_geom_list):
                 d2 = (data[3].c[0] - data[4].c[0]) **2 + (data[3].c[1] - data[4].c[1]) **2
             outsum += (d1 - d2) **2
             continue
+
         if data[0] == "col":
             outsum += (data[1].c[0] * data[2].c[1] + data[2].c[0] * data[3].c[1] + data[3].c[0] * data[1].c[1] - data[1].c[1] * data[2].c[0] - data[2].c[1] * data[3].c[0] - data[3].c[1] * data[1].c[0]) ** 2
             continue
+
         if data[0] == "cyc":
             outsum += (cyclic_det(data[2].c[0]-data[1].c[0], data[2].c[1]-data[1].c[1], data[3].c[0]-data[1].c[0], data[3].c[1]-data[1].c[1], data[4].c[0]-data[1].c[0], data[4].c[1]-data[1].c[1])) ** 2
+            continue
         
         if data[0] == "para":
             outsum += ((data[1].c[1] - data[2].c[1]) * (data[3].c[0] - data[4].c[0]) - (data[3].c[1] - data[4].c[1]) * (data[1].c[0] - data[2].c[0])) ** 2
+            continue
         
         if data[0] == "perp":
             outsum += ((data[1].c[1] - data[2].c[1]) * (data[3].c[1] - data[4].c[1]) + (data[3].c[0] - data[4].c[0]) * (data[1].c[0] - data[2].c[0])) ** 2
+            continue
 
         if data[0] == "online":
             outsum += (data[1].c[0] * data[2].c[0] + data[1].c[1] * data[2].c[1] + data[2].c[2]) ** 2
+            continue
         
         if data[0] == "oncirc":
             outsum += ((data[1].c[0] - data[2].c[0])**2 + (data[1].c[1] - data[2].c[1])**2 - data[2].c[2]**2) ** 2
-        
             continue
+
+        if data[0] == "eqangle":
+            dx1 = x[2].c[0] - x[1].c[0]
+            dy1 = x[2].c[1] - x[1].c[1]
+            dx2 = x[4].c[0] - x[3].c[0]
+            dy2 = x[4].c[1] - x[3].c[1]
+            dx3 = x[6].c[0] - x[5].c[0]
+            dy3 = x[6].c[1] - x[5].c[1]
+            dx4 = x[8].c[0] - x[7].c[0]
+            dy4 = x[8].c[1] - x[7].c[1]
+            outsum += ((dy1 * dx2 - dy2 * dx1) * (dy3 * dy4 + dx3 * dx4) - (dy3 * dx4 - dy4 * dx3) * (dy1 * dy2 + dx1 * dx2)) ** 2
+            continue
+
         if data[0] == "Formula":
             outsum += Explainer.calculate(data[1], in_geom_list) ** 2
     return outsum
 
-def calc_value(datum, inlst, vl, vnl):
+def update_and_calc_value(datum, inlst, vl, vnl):
     for obj_num in range(len(vl)):
         if vnl[obj_num] == 0:
             vl[obj_num].freec = inlst[obj_num]
@@ -81,7 +102,7 @@ def update_value(inlst, vl, vnl):
             vl[obj_num].freec = (inlst[obj_num], inlst[obj_num + 1])
     vl[0].tree.calc_all()
 
-DELTA = 3e-5
+DERIVATIVE_DELTA = 3e-5
 FIRST_STEP = 1e-6
 BIGTIMES = 3
 TIMES = 70
@@ -90,6 +111,8 @@ STEP = FIRST_STEP
 
 def descent(in_datum, in_tree):
     datum = in_datum
+    # Create a list of objects with free variables 
+    # Variable_num_list: 0 if degree_of_freedom == 1; 1, 2 the freec tuple when degree_of_freedom == 2
     variable_list = []
     variable_num_list = []
     for obj in in_tree.get_movable():
@@ -101,6 +124,7 @@ def descent(in_datum, in_tree):
             variable_num_list += [0]
     for bigtimer in range(BIGTIMES):
         
+        # The list of values of the free variables
         value_list = get_value(variable_list, variable_num_list)
         last_grad_list = []
         last_value_list = []
@@ -111,31 +135,31 @@ def descent(in_datum, in_tree):
 
             grad_list = []
             for obj_num in range(len(value_list)):
-                value_list0 = value_list.copy()
-                value_list0[obj_num] -= DELTA * 2
-                fmm = calc_value(datum, value_list0, variable_list, variable_num_list)
+                # Calculate the partial derivatives using values at 4 points
+                value_list_shifted = value_list.copy()
+                value_list_shifted[obj_num] -= DERIVATIVE_DELTA * 2
+                fmm = update_and_calc_value(datum, value_list_shifted, variable_list, variable_num_list)
 
-                value_list0 = value_list.copy()
-                value_list0[obj_num] -= DELTA
-                fm = calc_value(datum, value_list0, variable_list, variable_num_list)
+                value_list_shifted = value_list.copy()
+                value_list_shifted[obj_num] -= DERIVATIVE_DELTA
+                fm = update_and_calc_value(datum, value_list_shifted, variable_list, variable_num_list)
 
-                value_list0 = value_list.copy()
-                value_list0[obj_num] += DELTA
-                fp = calc_value(datum, value_list0, variable_list, variable_num_list)
+                value_list_shifted = value_list.copy()
+                value_list_shifted[obj_num] += DERIVATIVE_DELTA
+                fp = update_and_calc_value(datum, value_list_shifted, variable_list, variable_num_list)
 
-                value_list0 = value_list.copy()
-                value_list0[obj_num] += DELTA * 2
-                fpp = calc_value(datum, value_list0, variable_list, variable_num_list)
+                value_list_shifted = value_list.copy()
+                value_list_shifted[obj_num] += DERIVATIVE_DELTA * 2
+                fpp = update_and_calc_value(datum, value_list_shifted, variable_list, variable_num_list)
 
-                der = ((fmm - fpp) - (fm - fp)*8) / (12 * DELTA)
+                der = ((fmm - fpp) - (fm - fp)*8) / (12 * DERIVATIVE_DELTA)
                 grad_list.append(der)
                 update_value(value_list, variable_list, variable_num_list)
-            
-            # print(timer, grad_list)
 
             if timer == 0:
                 STEP = FIRST_STEP
             else:
+                # The Barzilar-Borwein Step
                 delta_value_list = []
                 for obj_num in range(len(value_list)):
                     delta_value_list.append(value_list[obj_num] - last_value_list[obj_num])
@@ -147,10 +171,9 @@ def descent(in_datum, in_tree):
                     return timer + bigtimer * TIMES
                 B = sum(delta_grad_list[_] * delta_grad_list[_] for _ in range(len(grad_list)))
                 STEP = abs(A / B)
-                # print(timer, delta_grad_list, grad_list, last_grad_list, delta_value_list, value_list, last_value_list, A, B, STEP)
 
+            # Update values of free variables
             last_value_list = value_list.copy()
-
             for obj_num in range(len(value_list)):
                 value_list[obj_num] -= STEP * grad_list[obj_num]
             update_value(value_list, variable_list, variable_num_list)
